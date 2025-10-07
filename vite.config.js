897c7b0ba7d7f1e79d29e1af9c7a0c130e106de4
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-10-07 16:17:06 +0530

    deployed v1

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
+const repoName = "VishwAlpha-EdTech-Platform"; 
+
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  base: `/${repoName}/`, 
   build: {
-    outDir: "build",
+    outDir: "build", 
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
-    port: "4028",
+    port: 4028,
     host: "0.0.0.0",
     strictPort: true,
     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
