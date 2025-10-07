6138c1c8e32ac08fb0830f84d49074d0ce21ad2a
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-10-07 16:12:30 +0530

    added all teh required features

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
-import ScrollToTop from "components/ScrollToTop";
-import ErrorBoundary from "components/ErrorBoundary";
-import NotFound from "pages/NotFound";
-import Services from './pages/services';
-import Contact from './pages/contact';
-import InternationalPresence from './pages/international-presence';
-import About from './pages/about';
-import Activities from './pages/activities';
-import Homepage from './pages/homepage';
+import Routes from "./Routes";
 
-const Routes = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <ErrorBoundary>
-      <ScrollToTop />
-      <RouterRoutes>
-        {/* Define your route here */}
-        <Route path="/" element={<Homepage />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/international-presence" element={<InternationalPresence />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="*" element={<NotFound />} />
-      </RouterRoutes>
-      </ErrorBoundary>
-    </BrowserRouter>
+    <Routes />
   );
-};
+}
 
-export default Routes;
+export default App;
